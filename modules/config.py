1cb2ba6bb210cec1ed35741ab01909e1fed964f9
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2021-07-03 22:18:51 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,21 @@
+
+#=================================\CONFIG./=====================================
+# default System camera access code = 1, if connect with any external camera put 1,2 and so on with the no of connected cameras.
+camera_no = 0
+
+
+# To count the total number of people (True/False).
+People_Counter = True
+
+# Set the threshold value for total violations limit.
+Threshold = 15
+
+# Set if GPU should be used for computations; Otherwise uses the CPU by default.
+USE_GPU = True
+
+MIN_CONF = 0.6
+
+NMS_THRESH = 0.3
+#===============================================================================
+# Face threshold
+confidence_threshold = 0.4
